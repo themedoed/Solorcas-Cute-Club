aec83091b95d93972000a01c0d8d7aceb2b964f8
themedoed <93190084+themedoed@users.noreply.github.com>
2021-12-09 23:14:29 +0700

    Адаптив и Кнопки
    - Сделал адаптив
    - Поменял местами кнопки Buy и Mint
    - Вместо Learn More - Minted

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(function(){
-
+    $('.solorcas__slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 530,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    vertical: true,
+                    verticalSwiping: true
+                }
+            }
+        ]
+    });
 });
